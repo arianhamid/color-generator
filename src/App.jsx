e2cd6d2814cb3f0f2575e16a93753048ce4f571e
Hamid <hamidarianyegane@gmail.com>
2023-07-23 01:14:39 +0330

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import SingleColor from "./SingleColor";
 
 import Values from "values.js";
 function App() {
-  const [color, setColor] = useState("#f15025");
+  const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
   const submitHandler = (e) => {
+    e.preventDefault();
     try {
-      const colors = new Values(color).all(10);
+      let colors = new Values(color).all(10);
       setList(colors);
     } catch (error) {
       setError(true);
       console.log(error);
     }
-    e.preventDefault();
-    setColor(e.target.value);
   };
   return (
     <>
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -2,14 +2,29 @@ import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = (color) => {
+  const [alert, setAlert] = useState(false);
   const { rgb, weight } = color;
   const bcg = rgb.join(",");
-
+  const hex = rgbToHex(...rgb);
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeOut);
+  }, [alert]);
   return (
     <article
       className="color"
       style={{ backgroundColor: `rgb(${bcg})` }}
-    ></article>
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
+    >
+      <p className="precent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
   );
 };
 
